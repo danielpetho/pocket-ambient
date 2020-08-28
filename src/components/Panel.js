0950cd9a7b9263c2abafcfab18a8469d28dcbeb2
emzeperix <kpethodaniel@gmail.com>
2020-08-28 10:41:26 +0200

    2nd commit

---
+++ src/components/Panel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 50%;
+    background: rgb(5,5,5);
+    background: linear-gradient(180deg, rgba(5,5,5,1) -17%, rgba(40,5,73,1) 100%);
+    border-radius: 10px;
+`
+
+const Panel = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default Panel;
+
